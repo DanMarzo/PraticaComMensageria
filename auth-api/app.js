7ac7a81b334218261cb6572c6e82cb3ab286137b
DanMarzo <marzogildan@gmail.com>
2024-08-26 22:06:14 -0300

    Feat: incluindo rotas

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,18 +1,19 @@
 import express from "express";
 import {createInitialData} from "./src/config/db/initialData.js";
+import routes from "./src/controllers/router.js"
+
 
 const app = express();
 const env = process.env;
 const port = env.PORT || 8080;
 
-
 createInitialData();
 
+app.use(routes)
 app.get("/api/status", (req, res) => {
   return res.json({ service: "auth api online" });
 });
 
 app.listen(port, () => {
   console.info("porta " + port);
-
 });
