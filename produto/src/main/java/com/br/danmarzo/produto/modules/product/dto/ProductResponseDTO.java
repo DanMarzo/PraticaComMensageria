5f82880752b9af4f80bb717fb7a1d88cd77b9d75
DanMarzo <marzogildan@gmail.com>
2024-09-15 16:28:27 -0300

    Arrumando response do produto

--- produto/src/main/java/com/br/danmarzo/produto/modules/product/dto/ProductResponseDTO.java
+++ produto/src/main/java/com/br/danmarzo/produto/modules/product/dto/ProductResponseDTO.java
@@ -1,18 +1,34 @@
 package com.br.danmarzo.produto.modules.product.dto;
 
 import com.br.danmarzo.produto.domain.ProductEntity;
+import com.br.danmarzo.produto.modules.category.dto.CategoryResponseDTO;
+import com.br.danmarzo.produto.modules.supplier.dto.SupplierResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductResponseDTO {
     private Integer id;
     private String name;
+    private Integer quantityAvailable;
+    private SupplierResponseDTO supplier;
+    private CategoryResponseDTO category;
 
     public static ProductResponseDTO of(ProductEntity product){
-        var response = new ProductResponseDTO();
-        BeanUtils.copyProperties(product, response);
-        return response;
+        return ProductResponseDTO
+                .builder()
+                .id(product.getId())
+                .name(product.getName())
+                .quantityAvailable(product.getQuantityAvailable())
+                .supplier(SupplierResponseDTO.of(product.getSupplier()))
+                .category(CategoryResponseDTO.of(product.getCategory()))
+                .build();
     }
 }
 
