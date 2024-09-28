6c35ba2e7e9e119874d9c417cc06394ec6c71b33
DanMarzo <marzogildan@gmail.com>
2024-09-28 19:38:25 -0300

    Alterando secrest

--- auth-api/src/Infra/constants/secrets.js
+++ auth-api/src/Infra/constants/secrets.js
@@ -1,3 +1,3 @@
 const env = process.env;
 
-export const apiSecret = env.API_SECRET ?? "YmY3ZWYwNDQtODdlMy00NWIwLThjYzktOGYwYTcyOTQwOGRjX2Rhbm1hcnpvXzE0NWY4NTg4LTA1MWMtNGZmMC04ZjhjLTc2OGNmMDU1NmMyNw==";
+export const apiSecret = env.API_SECRET ?? "424e0edc-4ffe-4abb-aa35-788fbebc39fa-5e705d1d-89b1-4868-aade-0267b199a774--d0463d9a-ebab-4b55-af16-91c35606e20f";
