96bc84e5ba8760aaa0755549b3d89dbe948b4afa
DanMarzo <marzogildan@gmail.com>
2024-09-28 19:31:54 -0300

    Incluindo configuração

--- sales-api/src/app.module.ts
+++ sales-api/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('CONNECT_MONGO'), // Pega a variável do .env
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
