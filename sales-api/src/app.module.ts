436603917c6ac9be163959f9f35cb464983979aa
DanMarzo <marzogildan@gmail.com>
2024-09-28 19:47:20 -0300

    Preparando services

--- sales-api/src/app.module.ts
+++ sales-api/src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthGuardService } from './modules/auth/auth-guard/auth-guard.service';
+import { AuthService } from './modules/auth/auth.service';
+import { SalesService } from './modules/sales/sales.service';
+import { SalesModule } from './modules/sales/sales.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthGuardModule } from './modules/auth/auth-guard/auth-guard.module';
+import { SalesModule } from './modules/sales/sales.module';
 
 @Module({
   imports: [
@@ -12,8 +20,17 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: configService.get<string>('CONNECT_MONGO'), // Pega a variável do .env
       }),
     }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('SECRET_KEY'), // Pega a variável do .env
+      }),
+    }),
+    SalesModule,
+    AuthGuardModule,
+    AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [AuthGuardService, AuthService, SalesService],
 })
 export class AppModule {}
