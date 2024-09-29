66e0451dc244a4785c3d40bf00c0babdaf5cb054
DanMarzo <marzogildan@gmail.com>
2024-09-29 13:53:39 -0300

    Entidades

--- sales-api/src/domain/order.entity.ts
+++ sales-api/src/domain/order.entity.ts
@@ -1,12 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { ProductEntity } from './product.entity';
+import { UserEntity } from './user.entity';
 
 export type OrderDocument = HydratedDocument<Order>;
 
 @Schema()
 export class Order {
-  @Prop({ isRequired: true })
+  @Prop({ isRequired: true, type: Object })
+  user: UserEntity;
+  @Prop({ isRequired: true, type: Array })
+  products: Array<ProductEntity>;
+  @Prop({ isRequired: true, type: String })
   status: string;
+  @Prop({ isRequired: true, type: Date })
+  createAt: Date;
+  @Prop({ isRequired: true, type: Date })
+  updateAt: Date;
 }
 
 export const OrderSchema = SchemaFactory.createForClass(Order);
