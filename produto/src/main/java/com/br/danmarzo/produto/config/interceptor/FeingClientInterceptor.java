8f86130deff6e5fedd28fef39869ae65ad2f1d8e
DanMarzo <marzogildan@gmail.com>
2024-10-23 23:24:37 -0300

    Simplificando debug com variaveis

--- produto/src/main/java/com/br/danmarzo/produto/config/interceptor/FeingClientInterceptor.java
+++ produto/src/main/java/com/br/danmarzo/produto/config/interceptor/FeingClientInterceptor.java
@@ -4,22 +4,27 @@ import com.br.danmarzo.produto.config.exception.ValidationException;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+@Component
 public class FeingClientInterceptor  implements RequestInterceptor {
     private static final String Authorization = "Authorization";
+
     @Override
     public void apply(RequestTemplate template) {
         var httpRequest = this.getCurrentRequest();
-        template.header(Authorization, httpRequest.getHeader(Authorization));
+        var token = httpRequest.getHeader(Authorization);
+        template.header(Authorization, token);
     }
 
     private HttpServletRequest getCurrentRequest(){
         try {
-            return ((ServletRequestAttributes)RequestContextHolder
+            var req =((ServletRequestAttributes)RequestContextHolder
                     .getRequestAttributes())
                     .getRequest();
+            return req;
         } catch (Exception e) {
             e.printStackTrace();
             throw new ValidationException("A requisicao atual nao foi processada");
